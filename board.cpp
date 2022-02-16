db2c73fcfac9faddfb33e89eaf794e371004a828
NeZE <boss-NeZE@hotmail.com>
2022-02-16 21:59:21 +0700

    11

--- board.cpp
+++ board.cpp
@@ -1,84 +1,103 @@
-#include<iostream>
-
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include "board.h"
 using namespace std;
-const int N = 9; // เปลี่ยนได้ตามขนาด นั้นนี้ตั้งค่าเริ่มต้นไว้เฉยๆ
-const int M = 9;
 
-void pullpiecse(bool [][M], int,int);
+void chooseboardsize();
+void printboard(int,int);
+void setstone(int);
+int a;
+int board[1000][1000];
+void chooseboardsize(){
+    do{
+    cout <<  "Choose board size (9,13,19): ";
+    cin >> a;
+    if(a != 9&&a != 13&&a != 19) cout << "Please input correct number" << endl;
+    }while(a != 9&&a != 13&&a != 19);
+    if(a==9){
+        for(int x=0;x<11;x++){
+            for(int y=0;y<11;y++){
+                board[y][x] = board_9x9[y][x];
+            }
+        }
+    }
+    if(a==13){
+        for(int x=0;x<15;x++){
+            for(int y=0;y<15;y++){
+                board[y][x] = board_13x13[y][x];
+            }
+        }
+    }
+    if(a==19){
+        for(int x=0;x<21;x++){
+            for(int y=0;y<21;y++){
+                board[y][x] = board_19x19[y][x];
+            }
+        }
+    }
+}
+void setstone1(){
+    int x,y;
+    cout << "player 1: : ";
+    cin >> x >> y;
+    while(board[x][y] !=0){
+        cout << "Alredy" << endl;
+        cout << "player 1 : ";
+        cin >> x >> y;
+    }
+    board[x][y] = 1;
+}
+void setstone2(){
+    int x,y;
+    cout << "player 2 : ";
+    cin >> x >> y;
+   while(board[x][y] !=0){
+        cout << "Alredy"<< endl;
+        cout << "player 2 : ";
+        cin >> x >> y;
+    }
+    board[x][y] = 2;
+}
+void printboard(int a,int board[][1000]){
+    for(int i=0;i<a+1;i++){
+        if(i !=0)
+        cout << i <<'\t';
+        for(int y =0;y<a+2;y++){
+            if(board[i][y]==0) cout << "."  << " ";
+            if(board[i][y]==1) cout << "x"  << " ";
+            if(board[i][y]==2) cout << "y"  << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
-void pull(const bool [][M]);
-
-void board(const bool[][M],const bool[][M]);
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
 
 int main(){
-    bool player1[N][M] = {};
-    bool player2[N][M] = {};
-    int x, y,w,z,g;
-
-        do{
-            board(player1,player2);
-            cout << "Player1 Input your location: ";
-            do{
-                cin >> x >> y;
-                if(player1[x-1][y-1]==1 || player2[x-1][y-1] == 1)
-                    cout << "Alredy Exist"<<endl << "Player1 Input your location: ";
-            }while(player1[x-1][y-1]==1|| player2[x-1][y-1] == 1);
-            player1[x-1][y-1] = 1;
-           board(player1,player2);
-            cout << "Player2 Input your location: ";
-            do{
-                cin >> w >> z;
-                if(player1[w-1][z-1]==1 || player2[w-1][z-1] == 1)
-                    cout << "Alredy Exist"<<endl << "Player2 Input your location: ";
-            }while(player1[w-1][z-1]==1|| player2[w-1][z-1] == 1);
-            player2[w-1][z-1] = 1;
-           board(player1,player2);
-           cout << endl<< "0 to stop ";
-           cin >> g;
-           cout << endl;
-        }while(g!=0);
-           
-
-      
-    return 0;
+    int g;
+    chooseboardsize();
+    do{
+    printboard(a,board);
+    setstone1();
+    printboard(a,board);
+    setstone2();
+    printboard(a,board);
+    cout << "0 to stop : ";
+    cin >> g;
+    }while(g !=0);
 }
-
-
-void board(const bool A[][M],const bool B[][M]){
-    for(int i=0;i<M;i++){
-        cout << i+1 << "  ";
-        for (int j=0;j<M;j++){
-            if(A[j][i])
-                cout << " *";
-            if(B[j][i])
-                cout << " #";
-            if(A[j][i] ==0 && B[j][i]==0)
-                cout << " .";
-        }
-            cout << endl;
-
-    }
-    cout << "    ";
-    for(int i=0;i<M;i++){
-        cout << i+1 <<" ";
-    }
-    cout << "\n";
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
